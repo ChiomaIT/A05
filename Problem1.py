c2603578e3f0ef7b3f37b3f2c83e4c33c28c9897
ChiomaIT <chiomatheresa12345@gmail.com>
2023-03-16 20:42:09 +0000

    Adds Macaroni to file

--- Problem1.py
+++ Problem1.py
@@ -1,20 +1,25 @@
-def addArray(numList, ind, val):
-  if ind >= 0:
-    val += numList[ind]
-    return addArray(numList[:ind], ind-1,val)
-  return val
+def main():
+  def addArray(numList, ind, val):
+    if ind >= 0:
+      val += numList[ind]
+      return addArray(numList[:ind], ind-1,val)
+    return val
+  
+  numList = [1,2,3]
+  # print(addArray(numList, len(numList) - 1, 0))
+  
+  def removeNegative(nums, i, res):
+    if i >= 0:
+      if nums[i] > 0:
+        res.append(nums[i])
+        removeNegative(nums[:i], i - 1, res)
+      else:
+        removeNegative(nums[:i], i - 1, res)
+    return res
+  
+  nums = (1,2,-3,-5,6,2,3,44,5,6,-10,-1)
+  # print(removeNegative(nums, len(nums)-1, []))
 
-numList = [1,2,3]
-print(addArray(numList, len(numList) - 1, 0))
-
-def removeNegative(nums, i, res):
-  if i >= 0:
-    if nums[i] > 0:
-      res.append(nums[i])
-      removeNegative(nums[:i], i - 1, res)
-    else:
-      removeNegative(nums[:i], i - 1, res)
-  return res
-
-nums = (1,2,-3,-5,6,2,3,44,5,6,-10,-1)
-print(removeNegative(nums, len(nums)-1, []))
+  
+if __name__ == '__main__':
+    main()
